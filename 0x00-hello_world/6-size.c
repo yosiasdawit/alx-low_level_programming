9b13130ad7dcab0d383dd63690932f5dae0b3949
Yosias Dawit <yosiasdawit95@gmail.com>
2022-10-13 11:23:44 -0700

    aq

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of char: %1 bytes(s)\n", sizeof(char));
+	printf("Size of a long int: %1 bytes(s)\n", sizeof(long int));
+	printf("Size of a float: %1 bytes(s)\n",sizeof(float))
+	printf("Size of a long long int: %1 bytes(s)\n", sizeof(long long int));
+	return (0);
+}
