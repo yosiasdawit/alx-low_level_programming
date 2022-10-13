787cc1064d1d1a4b8e161464f33a71ace202df47
Yosias Dawit <yosiasdawit95@gmail.com>
2022-10-13 13:08:59 -0700

    ww

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ *
+ *  * Description: main - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
+
 int main(void)
+
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	ruturn (0);
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
 }
